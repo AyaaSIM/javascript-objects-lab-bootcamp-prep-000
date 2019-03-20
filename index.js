efa11989140bb191dfefb03205c0116ff3b06205
Ayaa <ayaa1siddig5@gmail.com>
2019-03-20 10:06:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
 var recipes = []
-function updateObjectWithKeyAndValue(object, key, value){
-  object[key]=value
-  call;{updateObjectWithKeyAndValue}
-}
+ function updateObjectWithKeyAndValue(obj, key, val) {
+  return Object.assign({}, obj, {[key]: val})
