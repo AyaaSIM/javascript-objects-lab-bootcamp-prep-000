549cec538ddf1460c0dc995c85f90e3d595260d7
Ayaa <ayaa1siddig5@gmail.com>
2019-03-20 10:08:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,18 @@ var recipes = {}
 function updateObjectWithKeyAndValue(object, key, value){
   return Object.assign({}, obj, {[key]: val})
 }
+
+ function destructivelyUpdateObjectWithKeyAndValue(obj, key, val) {
+  obj[key] = val;
+  return obj
+}
+
+ function deleteFromObjectByKey(obj, key) {
+  var recipesUp = Object.assign({}, obj)
+  delete recipesUp[key]
+  return recipesUp
+}
+
+ function destructivelyDeleteFromObjectByKey(obj, key) {
+  delete obj[key]
+  return obj
